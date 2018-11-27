afc47dcec61d600f42dcb85ffef787278108f3b3
Gabriela <gab.jo.cuello@gmail.com>
2018-11-27 16:45:33 -0500

    Initial commit, not complete

--- contact.rb
+++ contact.rb
@@ -1,18 +1,29 @@
 class Contact
+  ### CLASS VARIABLES
+  @@list= ["wprd ", 1, 2 ]
+  @@next_id = 1
 
   # This method should initialize the contact's attributes
-  def initialize
+  def initialize(first_name, last_name, email,notes = "")
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @notes = notes
+    @id = nil
 
-  end
+    end
 
-  # This method should call the initializer, 
+  # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
-
+  def self.create(first_name, last_name, email, notes = 'N/A')
+    new_contact = Contact.new(first_name, last_name, email, notes)
+    new_contact.save
+    new_contact
   end
 
   # This method should return all of the existing contacts
   def self.all
+    return @@list
 
   end
 
@@ -22,7 +33,7 @@ class Contact
 
   end
 
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
@@ -37,15 +48,55 @@ class Contact
   def self.find_by
 
   end
-
+# found = array.detect {|e| e == 1005}
   # This method should delete all of the contacts
   def self.delete_all
 
   end
+  ######  INSTANCE methods
+###### READERSSS
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def email
+    @email
+  end
+
+  def notes
+    @notes
+  end
 
   def full_name
-
+    "#{first_name} #{last_name}"
   end
+
+  ###WRITERSSSSS
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+
+  def last_name=(last_name)
+    @last_name = last_name
+  end
+
+  def email=(email)
+    @email = email
+  end
+
+  def notes=(notes)
+    @notes = notes
+  end
+
+  def save
+      @@next_id += 1
+      @id = @@next_id
+      @@list << self
+    end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
@@ -54,5 +105,14 @@ class Contact
   end
 
   # Feel free to add other methods here, if you need them.
-  
+
 end
+
+gab = Contact.create("Gabriela", "roa", "gab@roa@gmail.com","ahash")
+puts gab.full_name
+bag = Contact.create("Gabe", "roa", "gab@rty@gmail.com","ah")
+# puts Contact.all
+found = Contact.all.detect {|e| e == 1}
+{ |l| l.owner == myself }
+
+puts found
